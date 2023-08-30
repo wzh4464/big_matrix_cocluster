fa296d707507353d57029c77d653c3cc7c9c7d39
Hance_Wu_M1 <32484940+wzh4464@users.noreply.github.com>
2023-08-30 13:55:56 +0800

    WIP: for undo cython

--- coclusterSVD.py
+++ coclusterSVD.py
@@ -3,7 +3,7 @@ File: /coclusterSVD.py
 Created Date: Wednesday August 30th 2023
 Author: Zihan Wu
 -----
-Last Modified: Wednesday, 30th August 2023 10:33:03 am
+Last Modified: Wednesday, 30th August 2023 12:32:42 pm
 Modified By: the developer formerly known as Zihan Wu at <wzh4464@gmail.com>
 -----
 HISTORY:
@@ -12,7 +12,8 @@ Date      		By   	Comments
 '''
 
 # define class coclusterSVD with only methods (score, scoreHelper)
-from numpy import sum, abs, corrcoef, eye, min
+from numpy import sum, abs, corrcoef, eye, min, diag
+from numpy.linalg import svd
 
 def scoreHelper(length, C):
     return 1 - 1 / (length - 1) * sum(C, axis=1)
@@ -31,3 +32,13 @@ def score(X, subrowI : list, subcolJ : list):
     S2 = abs(corrcoef(X[subrowI, :][:, subcolJ].T) - eye(lenI))
 
     return min([scoreHelper(lenJ, S1), scoreHelper(lenI, S2)])
+
+def cocluster(X, scale, k) -> tuple:
+    # [U, ~, V] = svds(X, scale);
+    # X = U * S * V'
+    # U: m x scale
+    # S: scale x scale
+    # V: n x scale
+    U, S, V = svd(X, full_matrices=False)
+    s = diag(S)
+    print(s)
