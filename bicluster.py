f25dc3bcea37b1dc207af160d65dd557a682fd91
Hance_Wu_M1 <32484940+wzh4464@users.noreply.github.com>
2023-08-31 10:14:41 +0800

    9 but small

--- bicluster.py
+++ bicluster.py
@@ -3,7 +3,7 @@ File: /bicluster.py
 Created Date: Wednesday August 30th 2023
 Author: Zihan Wu
 -----
-Last Modified: Wednesday, 30th August 2023 9:49:19 pm
+Last Modified: Thursday, 31st August 2023 9:37:44 am
 Modified By: the developer formerly known as Zihan at <wzh4464@gmail.com>
 -----
 HISTORY:
@@ -14,12 +14,22 @@ Date      		By   	Comments
 from attr import dataclass
 import numpy as np
 
-@dataclass
+@dataclass(eq=False)
 class bicluster:
     row_idx: np.ndarray
     col_idx: np.ndarray
     score: float
     # data: np.ndarray
+    def __eq__(self, other) -> bool:
+        if other.__class__ is not self.__class__:
+            return NotImplemented
+        return (
+            np.all(self.row_idx == other.row_idx) 
+            and
+            np.all(self.col_idx == other.col_idx) 
+            # and
+            # self.score == other.score
+        )
 
     
 
