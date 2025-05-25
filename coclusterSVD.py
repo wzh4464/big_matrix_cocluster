abfdb3aab2afb4c4811fe99e7f1c49d1d2290da0
Hance_Wu_M1 <32484940+wzh4464@users.noreply.github.com>
2025-05-25 11:41:05 +0800

    row_idx and col_idx

--- coclusterSVD.py
+++ coclusterSVD.py
@@ -3,7 +3,7 @@ File: /coclusterSVD.py
 Created Date: Wednesday August 30th 2023
 Author: Zihan Wu
 -----
-Last Modified: Wednesday, 30th August 2023 3:31:42 pm
+Last Modified: Wednesday, 30th August 2023 3:54:20 pm
 Modified By: the developer formerly known as Zihan Wu at <wzh4464@gmail.com>
 -----
 HISTORY:
@@ -14,6 +14,8 @@ Date      		By   	Comments
 # define class coclusterSVD with only methods (score, scoreHelper)
 from numpy import sum, abs, corrcoef, eye, min
 from numpy.linalg import svd
+from sklearn.cluster import KMeans
+# import matplotlib.pyplot as plt
 
 def scoreHelper(length, C):
     return 1 - 1 / (length - 1) * sum(C, axis=1)
@@ -37,6 +39,16 @@ def cocluster(X, scale, k):
     # [U, S, V] = svds(X, scale);
     # s is a vector saving the singular values of X
     
-    U, S, V = svd(X, full_matrices=False)
-    print('changed')
-    return S
+    U, S, Vh = svd(X, full_matrices=False)
+    
+    # [row_idx, row_cluster, row_dist, row_sumd] = kmeans(U, k);
+    # [col_idx, col_cluster, col_dist, col_sumd] = kmeans(V, k); 
+    kmeans_U = KMeans(n_clusters=k, random_state=0, n_init='auto').fit(U)
+    kmeans_V = KMeans(n_clusters=k, random_state=0, n_init='auto').fit(Vh.T)
+    row_idx = kmeans_U.labels_
+    col_idx = kmeans_V.labels_
+    
+    print('row_idx', row_idx)
+    print('col_idx', col_idx)
+    
+    return
